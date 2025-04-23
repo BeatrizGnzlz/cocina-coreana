aee2b1057aaabc4027ff2a0277579b5a1dde749e
BeatrizGnzlz <beatrizgg46@gmail.com>
2025-04-23 16:51:43 +0200

    10

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,6 @@
 let likeCountJapchae = 0;
 let likeCountKimchi = 0;
+let likeCountKimbap = 0;
 
 document.getElementById('like-japchae').addEventListener('click', () => {
     likeCountJapchae++;
@@ -9,4 +10,8 @@ document.getElementById('like-japchae').addEventListener('click', () => {
 document.getElementById('like-kimchi').addEventListener('click', () => {
     likeCountKimchi++;
     document.getElementById('like-count-kimchi').textContent = likeCountKimchi;
+});
+document.getElementById('like-kimbap').addEventListener('click', () => {
+    likeCountKimbap++;
+    document.getElementById('like-count-kimbap').textContent = likeCountKimbap;
 });
